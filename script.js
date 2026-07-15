462a2d934b1c5dfd01a2918788a97e485b3035fd
hrittik702 <mmhrittik@gmail.com>
2026-07-15 19:58:07 +0530

    remove : generate hex and rgb

--- script.js
+++ script.js
@@ -12,46 +12,16 @@ const random = {
   hundred: () => {
     return Math.floor(Math.random() * 101);
   },
-  hex: () => {
-    let hex = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  },
-};
-
-const rgbColor = () => {
-  return `rgb(${random.rgb()},${random.rgb()},${random.rgb()})`;
 };
 
 const boxes = document.querySelectorAll('.box');
-
-const btnRGB = document.querySelector('#rgb-btn');
-btnRGB.addEventListener('click', () => {
-  let colors = [rgbColor(), rgbColor(), rgbColor(), rgbColor(), rgbColor()];
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].children[0].innerHTML = `${colors[i]}`;
-    boxes[i].style.backgroundColor = colors[i];
-  }
-});
-
-const btnHEX = document.querySelector('#hex-btn');
-btnHEX.addEventListener('click', () => {
-  let colors = [random.hex(), random.hex(), random.hex(), random.hex(), random.hex()];
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].children[0].innerHTML = `${colors[i]}`;
-    boxes[i].style.backgroundColor = colors[i];
-  }
-});
 
 // TODO :
 // 1. Use hsl instead of rgb and hex
 // 2. Interconversion of hex-rgb-hsl
 // 3. Use Generic color names as suggested!
 
-// CLOSE  on 1
+// WORKING   on 1
 
 const hslColor = {}; //0 = hue, 1 = sat, 2 = light
 
@@ -71,12 +41,11 @@ const btnHSL = document.querySelector('#hsl-btn');
 btnHSL.addEventListener('click', () => {
   let colors = [randomHSL(), randomHSL(), randomHSL(), randomHSL(), randomHSL()];
   for (let i = 0; i < boxes.length; i++) {
-    boxes[i].children[0].innerHTML = `${colors[i]}`;
+    boxes[i].children[0].innerHTML = `${hexCode[i]}`;
     boxes[i].style.backgroundColor = colors[i];
   }
 });
 
-// CLOSE  - hsl to rgb
 const hslToRgb = (h, s, l) => {
   h /= 60;
   s /= 100;
